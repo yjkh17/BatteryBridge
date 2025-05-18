00d0be331e3d7b246638a4a2d7a8bb0c3f5716bf
yjkh17 <yj.kh17@gmail.com>
2025-05-18 22:53:14 +0300

    Initial Commit

---
+++ BatteryBridge/BatteryBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BatteryBridgeApp.swift
+//  BatteryBridge
+//
+//  Created by Yousef Jawdat on 18/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct BatteryBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
